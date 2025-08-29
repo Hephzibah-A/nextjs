31173638102d59150a6f8cd7c46be3cc6eee08f8
Hephzibah A <hephzibahantony11@gmail.com>
2025-08-29 21:16:01 +0530

    Create Dashboard Project

---
+++ Projects/demos/src/app/users/profile/about-user/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutUser = () => {
+  return <div>This is the About User route. Sub-route of Users-Profile</div>;
+};
+
+export default AboutUser;
---
+++ Projects/demos/src/app/users/profile/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Profile = () => {
+  return <div>This is the Profile route. Sub-route of Users</div>;
+};
+
+export default Profile;
